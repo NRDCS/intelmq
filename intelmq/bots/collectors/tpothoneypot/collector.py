6bf7f7e5ee2f4d2ba3821edd9e21aa2a9a6005fe
Aurimas Rudinskis <arudinskis@gmail.com>
2018-05-22 20:32:22 +0300

    ENH: T-POT collector (elasticsearch)

---
+++ intelmq/bots/collectors/tpothoneypot/collector.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+"""
+T-POT honeypot report collector bot
+Queries ElasticSearch to get events from T-POT honeypots.
+
+Parameters:
+    query_file: string
+    period_to_query: int
+    honeypot_count: int
+    connection_value_sum: int
+    classification: string
+"""
+
+import requests, json
+from intelmq.lib.bot import CollectorBot
+
+
+class HoneypotCollectorBot(CollectorBot):
+
+    def process(self):
+        query_file = self.parameters.query_file
+        period_to_query = self.parameters.period_to_query
+        honeypot_count = self.parameters.honeypot_count
+        connection_value_sum = self.parameters.connection_value_sum
+        classification = self.parameters.classification
+
+        elk_url = 'http://10.10.100.238:9200/_search'
+        headers = {'Content-Type': 'application/json'}
+
+        elk_query = json.load(open(query_file))
+        for item in elk_query['query']['bool']['must']:
+            if 'range' in item:
+                for key, value in item.items():
+                    for k, v in value.items():
+                        v['gte'] = 'now-' + str(period_to_query) + 'h'
+
+        self.logger.info('Honeypot to IntelMQ processing started')
+        response = requests.get(elk_url, data=json.dumps(elk_query), headers=headers, verify=False)
+        if response.status_code == 200 and 'took' in response.text:
+            honeypot_report = json.loads(response.text)
+            aggs = next(iter(honeypot_report['aggregations']))
+            for item in honeypot_report['aggregations'][aggs]['buckets']:
+                for key, value in item.items():
+                    if type(value) == dict:
+                        if len(value.get('buckets')) >= honeypot_count and item.get('doc_count') >= connection_value_sum:
+                            line = {}
+                            line.update({'source.ip': item.get('key')})
+                            line.update({'classification.type': classification})
+                            report = self.new_report()
+                            report.add('raw', json.dumps(line))
+                            self.send_message(report)
+        else:
+            self.logger.error(response.text)
+
+
+BOT = HoneypotCollectorBot
