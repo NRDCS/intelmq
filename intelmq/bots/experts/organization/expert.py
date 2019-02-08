afda2c851af724d39e763360431075fd9de201d8
Marius Urkis <marius@urkis.lt>
2019-02-08 18:10:18 +0200

    BUG: event overwrite added

--- intelmq/bots/experts/organization/expert.py
+++ intelmq/bots/experts/organization/expert.py
@@ -35,8 +35,8 @@ class OrganizationExpertBot(Bot):
             by_name = response.json()
             if 'message' not in by_name:
                 for line in by_name['data']:
-                    event.add('destination.organization.sector', line['sector_type']['name'])
-                    event.add('destination.organization.id', line['public_id'])
+                    event.add('destination.organization.sector', line['sector_type']['name'], overwrite=True)
+                    event.add('destination.organization.id', line['public_id'], overwrite=True)
 
         if 'source.organization.name' in event and 'source.organization.sector' not in event:
             req_body = {'with_deleted': True, 'search': str(event['source.organization.name']), 'user_name': api_user, 'password': api_pass}
@@ -44,8 +44,8 @@ class OrganizationExpertBot(Bot):
             by_name = response.json()
             if 'message' not in by_name:
                 for line in by_name['data']:
-                    event.add('source.organization.sector', line['sector_type']['name'])
-                    event.add('source.organization.id', line['public_id'])
+                    event.add('source.organization.sector', line['sector_type']['name'], overwrite=True)
+                    event.add('source.organization.id', line['public_id'], overwrite=True)
 
         # If IP is only value, then queries to get organisation ID
         if 'destination.ip' in event and 'destination.organization.name' not in event and 'destination.organization.sector' not in event:
@@ -58,9 +58,9 @@ class OrganizationExpertBot(Bot):
                 response = requests.post(api_url + '/api/organisations/' + str(org_id), data=json.dumps(req_body), headers=headers, verify=False)
                 data = response.json()
                 if 'message' not in data:
-                    event.add('destination.organization.name', data['data']['name'])
-                    event.add('destination.organization.sector', data['data']['sector_type']['name'])
-                    event.add('destination.organization.id', data['data']['public_id'])
+                    event.add('destination.organization.name', data['data']['name'], overwrite=True)
+                    event.add('destination.organization.sector', data['data']['sector_type']['name'], overwrite=True)
+                    event.add('destination.organization.id', data['data']['public_id'], overwrite=True)
 
         if 'source.ip' in event and 'source.organization.name' not in event and 'source.organization.sector' not in event:
             self.logger.debug('Searching for: %s.', str(event['source.ip']))
@@ -75,9 +75,9 @@ class OrganizationExpertBot(Bot):
                     org_public_id = by_ip['data'][0]['public_id']
                     org_name = by_ip['data'][0]['name']
                     org_sector = by_ip['data'][0]['sector_type']['name']
-                    event.add('source.organization.name', org_name)
-                    event.add('source.organization.sector', org_sector)
-                    event.add('source.organization.id', org_public_id)
+                    event.add('source.organization.name', org_name, overwrite=True)
+                    event.add('source.organization.sector', org_sector, overwrite=True)
+                    event.add('source.organization.id', org_public_id, overwrite=True)
                     self.logger.debug('Information added: Organization=%s, Sector=%s, id=%s', org_name, org_sector, org_public_id)
                     #response = requests.post(api_url + '/api/organisations/' + str(org_id), data=json.dumps(req_body), headers=headers, verify=False)
                     #data = response.json()
@@ -103,9 +103,9 @@ class OrganizationExpertBot(Bot):
                                 org_public_id = by_asn['data'][0]['public_id']
                                 org_name = by_asn['data'][0]['name']
                                 org_sector = by_asn['data'][0]['sector_type']['name']
-                                event.add('source.organization.name', org_name)
-                                event.add('source.organization.sector', org_sector)
-                                event.add('source.organization.id', org_public_id)
+                                event.add('source.organization.name', org_name, overwrite=True)
+                                event.add('source.organization.sector', org_sector, overwrite=True)
+                                event.add('source.organization.id', org_public_id, overwrite=True)
                                 self.logger.debug('Information added: Organization=%s, Sector=%s, id=%s', org_name, org_sector, org_public_id)
                             else:
                                 self.logger.debug('Did not retrieve data by ASN')
